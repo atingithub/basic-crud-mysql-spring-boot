3ed13a67ecff3966d6ca9679759f9b3bb9c4ca59
atin <atin.becst@gmail.com>
2020-07-18 15:07:35 +0530

    initial commit

---
+++ src/main/java/crud/atin/in/BasicCrudMysqlSpringBootApplication.java
@@ -0,0 +1,13 @@
+package crud.atin.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrudMysqlSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCrudMysqlSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crud/atin/in/BasicCrudMysqlSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package crud.atin.in;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicCrudMysqlSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
